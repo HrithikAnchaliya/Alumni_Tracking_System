958d6c4ab65d59b48a519d217f2147275a5c29cf
Hrithik Anchaliya <32984102+HrithikAnchaliya@users.noreply.github.com>
2020-03-29 11:55:47 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import Login from './Institute/Login/login.js';
 import Top from './Institute/Navigationbar/navbar.js';
 import Profilepage from './Institute/Profile/profilepage.js';
 import Searchprofile from './Institute/Profile/searchprofile.js'
-import Events from './Institute/Event/events.js'
-import Addevents from './Institute/Event/addevent.js'
-import Eventpage from './Institute/Event/eventpage.js'
+import Events from './Institute/Event/events.js';
+import Addevents from './Institute/Event/addevent.js';
+import Eventpage from './Institute/Event/eventpage.js';
+import Jobs from './Institute/jobs/jobs.js';
+import Addjobs from './Institute/jobs/addjobs.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -29,6 +31,8 @@ export default class Routes extends React.Component {
             <Route path='/eventpage' component={Eventpage}/>
             <Route path='/addevent' component={Addevents}/>
             <Route path='/events' component={Events}/>
+            <Route path='/jobs' component={Jobs}/>
+            <Route path='/addjobs' component={Addjobs}/>
           </Switch>
         </Router>
       );
