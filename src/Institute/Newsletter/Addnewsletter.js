9b3afd76a412401415714509a398c6e4aa250fd1
Hrithik Anchaliya <32984102+HrithikAnchaliya@users.noreply.github.com>
2020-06-19 15:25:00 +0530

    Development Newletter mode
    newsletter is only for development.

--- src/Institute/Newsletter/Addnewsletter.js
+++ src/Institute/Newsletter/Addnewsletter.js
@@ -15,18 +15,31 @@ export default class Addnewsletter extends React.Component{
         file :  event.target.files[0]
     })}
 
-    onSubmit = (e) => {
+    onSubmit = async (e) => {
         e.preventDefault();
         const { file } = this.state;
         if(file.type === "application/pdf"){
             console.log("Uploading")
-            let data = new FormData()
-            data.append('files', file)
-                    const values = {
-                    method : "POST",
-                    body : data
-                }
-                console.log(values)
+            let data = file
+            // data.append('files', file)
+
+            const values = {
+                method : "POST",
+                body : data
+            }
+            try{
+            const response = await fetch('http://localhost:4000/college/newsletters',values)
+            const json = await response.json()
+            if(!response.ok){
+                throw new Error(response.status); // 404
+            }
+            console.log(json)
+        }
+            catch(error){
+                console.log(error)
+                
+            }
+                // console.log(values)
                 // for (let pair of data.entries()) {
                 //     console.log(pair[0]+ ', ' + pair[1]); 
                 // }
