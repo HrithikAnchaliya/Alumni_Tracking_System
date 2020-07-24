f20d37d5787ec34c979c8be722443745ed2bcdf0
Hrithik Anchaliya <32984102+HrithikAnchaliya@users.noreply.github.com>
2020-07-24 19:13:41 +0530

    Delete endpoint.js

--- Endpoint/endpoint.js
+++
@@ -1,6 +0,0 @@
-// import store from '../Redux/store/storage'
-
-// let user = JSON.parse(localStorage.getItem('Auth_user'))
-
-// export let base_url_user = `https://alumni-backend-app.herokuapp.com/${user}`
-export let base_url = 'https://alumni-backend-app.herokuapp.com'
