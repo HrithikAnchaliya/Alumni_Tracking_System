1d7a55613d59ff4d2d03434245f91e4de88346d3
JatinRanka <37477429+JatinRanka@users.noreply.github.com>
2020-07-31 18:28:21 +0530

    Tickets page updated :heavy_plus_sign:

--- src/Institute/Tickets/Tickets.js
+++ src/Institute/Tickets/Tickets.js
@@ -4,6 +4,8 @@ import { notifyError_with_msg } from '../Utils/Message'
 import { connect } from 'react-redux'
 import Spinner from 'react-bootstrap/Spinner'
 import TicketCard from './TicketCard';
+import { Link } from "react-router-dom";
+
 
 class Tickets extends React.Component{
     constructor(props){
@@ -11,8 +13,22 @@ class Tickets extends React.Component{
         this.state = {
             tickets:null,
             loading:true,
-            error:false
-        }
+            error:false,
+            status: 'all'
+        };
+
+        this.onChangeStatus = this.onChangeStatus.bind(this);
+    }
+
+    getDate(date){
+        var updatedDate = new Date(date);
+        return updatedDate.getDate() + '/' + (updatedDate.getMonth()+1) + '/' + updatedDate.getFullYear()
+    }
+
+    onChangeStatus(event){
+        this.setState({
+            status: event.target.value
+        });
     }
 
     componentDidMount = async () => {
@@ -43,14 +59,96 @@ class Tickets extends React.Component{
 
     render(){
         console.log(this.state.tickets);
+
         return(
             <div>
                 <div className="container div-Container">
                 <div className="notification" id="jobcard-div">
                 {!this.state.loading ? 
                 (
-                    <div>{this.state.tickets.map((item,number) => <TicketCard key={number} title={item.title} sub={item.subTitle} id={item._id}/>)}</div>
-                ) : (
+                    // <div>{this.state.tickets.map((item,number) => <TicketCard key={number} title={item.title} sub={item.subTitle} id={item._id}/>)}</div>
+                    <div>
+
+                        <table 
+                            is-striped
+                            is-bordered
+                            is-narrow
+                            is-hoverable
+                            className="table">
+                            <thead>
+                                <tr>
+                                    <th>Title</th>
+                                    <th>Sub Title</th>
+                                    <th>Raised By</th>
+                                    <th>Date</th>
+                                    <th>
+                                        <select
+                                            onChange={this.onChangeStatus}    
+                                        >
+                                            <option value='all'>All</option>
+                                            <option value='open'>Open</option>
+                                            <option value='onProgress'>On Progress</option>
+                                            <option value='closed'>Closed</option>
+                                        </select>
+                                    </th>
+
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {
+                                    this.state.tickets.map((ticket) => {
+                                        if ( !(this.state.status == 'all' || this.state.status == ticket.status) ){
+                                            return;
+                                        }
+
+                                        var spanClassName;
+                                        switch (ticket.status) {
+                                            case "open":
+                                                spanClassName = "tag is-rounded is-success is-small"
+                                                break;
+                                            case "onProgress":
+                                                spanClassName = "tag is-rounded is-warning is-small"
+                                                break;
+                                            case "closed":
+                                                spanClassName = "tag is-rounded is-danger is-small" 
+                                                break;                                           
+                                        };
+
+                                        return (
+                                            <tr>
+                                                <td>{ticket.title}</td>
+                                                <td>{ticket.subTitle}</td>
+                                                <td>
+                                                    <Link to={`/profile/${ticket.postedBy._id}`}>
+                                                        {ticket.postedBy.firstName + " " + ticket.postedBy.lastName} 
+                                                    </Link>
+                                                </td>
+                                                <td>{ this.getDate(ticket.createdAt) }</td>
+                                                <td>
+                                                    <span className={spanClassName}>
+                                                        {ticket.status}
+                                                    </span>
+                                                </td>
+                                                <td>
+                                                    <Link to={`/tickets/${ticket._id}`}>
+                                                        Link
+                                                    </Link>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                                
+                            </tbody>
+
+                        </table>
+                        
+
+
+                    </div>
+                
+                    ) : (
                     (!this.state.error) ? (
                     <div id='Loading-id'>
                     <Spinner  animation="border" role="status">
